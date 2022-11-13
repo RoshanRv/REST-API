95538287303a981226845b8ac7f105c16c4c2b14
Roshan <nkroshankumar@gmail.com>
2022-11-13 07:44:22 +0530

    config updated

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         readonly MONGO_URI: string
+        readonly PRIVATE_ACCESS_KEY: string
+        readonly PUBLIC_ACCESS_KEY: string
+        readonly PRIVATE_REFRESH_KEY: string
+        readonly PUBLIC_REFRESH_KEY: string
     }
 }
