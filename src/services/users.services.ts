ab2e6c2dae7b92e23cca2bde23d23c6a7963192d
Roshan <nkroshankumar@gmail.com>
2022-11-13 07:19:06 +0530

    findUser() created

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,4 +1,6 @@
+import { omit } from "lodash"
 import mongoose, { DocumentDefinition } from "mongoose"
+import usersModel from "../models/users.model"
 import UsersModel, { UserType } from "../models/users.model"
 
 export const createUser = async (
@@ -11,4 +13,26 @@ export const createUser = async (
     } catch (e: any) {
         throw new Error(e)
     }
+}
+
+export const validateUser = async ({
+    email,
+    password,
+}: {
+    email: string
+    password: string
+}) => {
+    const user = await UsersModel.findOne({ email })
+
+    if (!user) return false
+
+    const isValid = await user.comparePassword(password)
+
+    if (!isValid) return false
+
+    return omit(user.toJSON(), "password")
+}
+
+export const findUser = async (userId: string) => {
+    return await usersModel.findById(userId)
 }
