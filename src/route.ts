74f2d285e41364584fc214cf4b3c6d206bda8048
Roshan <nkroshankumar@gmail.com>
2022-11-13 07:21:24 +0530

    added product, session routes in route.ts

--- src/route.ts
+++ src/route.ts
@@ -1,6 +1,25 @@
 import { Express, Request, Response } from "express"
+import {
+    deleteProductHandler,
+    createProductHandler,
+    getProductHandler,
+    updateProductHandler,
+} from "./controller/product.contoller"
+import {
+    createSessionHandler,
+    deleteSessionHandler,
+    getSessionHandler,
+} from "./controller/session.controller"
 import { createUserHandler } from "./controller/users.controller"
+import requireUser from "./middleware/requireUser"
 import validateResource from "./middleware/validateResources"
+import {
+    createProductSchema,
+    deleteProductSchema,
+    getProductSchema,
+    updateProductSchema,
+} from "./schema/product.schema"
+import { createSessionSchema } from "./schema/session.schema"
 import { createUserSchema } from "./schema/user.schema"
 
 const routes = (app: Express) => {
@@ -12,6 +31,37 @@ const routes = (app: Express) => {
         "/api/users",
         validateResource(createUserSchema),
         createUserHandler
+    )
+
+    app.post(
+        "/api/sessions",
+        validateResource(createSessionSchema),
+        createSessionHandler
+    )
+
+    app.get("/api/sessions", requireUser, getSessionHandler)
+
+    app.delete("/api/sessions", requireUser, deleteSessionHandler)
+
+    app.get(
+        "/api/products/:productId",
+        [requireUser, validateResource(getProductSchema)],
+        getProductHandler
+    )
+    app.post(
+        "/api/products",
+        [requireUser, validateResource(createProductSchema)],
+        createProductHandler
+    )
+    app.put(
+        "/api/products/:productId",
+        [requireUser, validateResource(updateProductSchema)],
+        updateProductHandler
+    )
+    app.delete(
+        "/api/products/:productId",
+        [requireUser, validateResource(deleteProductSchema)],
+        deleteProductHandler
     )
 }
 
