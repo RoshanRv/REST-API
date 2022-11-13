606674feb0f4510ef3c9f2850ee3e63754d09ad4
Roshan <nkroshankumar@gmail.com>
2022-11-13 07:43:23 +0530

    added middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,12 @@ dotenv.config()
 import logger from "./utils/logger"
 import connectDB from "./utils/connectDB"
 import routes from "./route"
+import deserializedUser from "./middleware/deserializedUser"
 
 const PORT = config.get<number>("port")
 const app = express()
 app.use(express.json())
+app.use(deserializedUser)
 
 app.listen(PORT, async () => {
     logger.info(`Server is Running at ${PORT} `)
