94367c9dc4b6687c0c96e4d80ce5fb58da096c67
Roshan <nkroshankumar@gmail.com>
2022-11-13 07:00:44 +0530

    products.services created

---
+++ src/services/product.services.ts
@@ -0,0 +1,31 @@
+import {
+    DocumentDefinition,
+    FilterQuery,
+    QueryOptions,
+    UpdateQuery,
+} from "mongoose"
+import ProductModel, { ProductType } from "../models/product.model"
+
+export const createProduct = async (
+    input: DocumentDefinition<Omit<ProductType, "createdAt" | "updatedAt">>
+) => {
+    return await ProductModel.create(input)
+}
+
+export const findProduct = async (query: FilterQuery<ProductType>) => {
+    return await ProductModel.findOne(query)
+}
+
+export const findProductAndUpdate = async (
+    query: FilterQuery<ProductType>,
+    update: UpdateQuery<ProductType>,
+    options: QueryOptions
+) => {
+    const updated = await ProductModel.findOneAndUpdate(query, update, options)
+    console.log(update)
+    return updated
+}
+
+export const deleteProduct = async (query: FilterQuery<ProductType>) => {
+    return await ProductModel.deleteOne(query)
+}
