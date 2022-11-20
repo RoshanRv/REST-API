316e5ef7b36f56d8060056cceeb77a74dc7e66ab
Roshan <nkroshankumar@gmail.com>
2022-11-20 22:57:45 +0530

    header created

---
+++ client/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="px-6 py-3 bg-gradient-to-tr from-purple-300 to-purple-600 shadow-md flex items-center justify-between">
+                <h1>Products</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
