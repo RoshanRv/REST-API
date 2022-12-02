f00f0d72ea32d42a23987a5ce43733153fde3f02
Roshan <nkroshankumar@gmail.com>
2022-12-02 12:57:58 +0530

    axios installed and origin updated

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,5 +1,5 @@
 export default {
-    origin: "http://localhost:3000",
+    origin: "https://rest-api-ui.vercel.app",
     port: process.env.PORT || 1337,
     saltWorkFactor: 10,
     accessTokenLife: "20m",
